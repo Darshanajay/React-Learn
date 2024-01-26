889433737630395a4bce8b5ffa062e8de55686b4
darshanajay <darshanajay60@gmail.com>
2024-01-26 23:31:42 +0000

    fregments

--- 18Fregments/learning-fregments/src/App.jsx
+++ 18Fregments/learning-fregments/src/App.jsx
@@ -1,7 +1,23 @@
-import './App.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 
 function App() {
-  return <h1>Fregments</h1>
+  let fooditems = ["Mudde", "Rotti", "Chikken", "Dose", "Idle", "Mlik","Ghee"];
+  return (
+    <React.Fragment>
+      <div>
+        <h1>Healthy Food</h1>
+      </div>
+      <ul className="list-group">
+        {fooditems.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </React.Fragment>
+  );
 }
 
-export default App
+export default App;
